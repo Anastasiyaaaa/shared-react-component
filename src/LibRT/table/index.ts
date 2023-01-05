569c4b573ce0e4bc825498641fcf2db03283f04a
Anastasiyaaaa <anastasiya.onishchenk@gmail.com>
2023-01-05 12:29:36 +0200

    test

--- src/LibRT/table/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './tableBody/TableBody';
-export * from './tableBody/TableColumn';
-export * from './tableBody/TableRow';
-export * from './tableHeader/TableHeader';
-export * from './tableHeader/TableRowHeader';
-export * from './tableHeader/TableColumnHeader';
-export * from './Table';
--- src/TableComp.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const TableComp = () => {
-  console.log('dddd')
-  return(<div>Table Table</div>)
-}
-
-export default TableComp;
