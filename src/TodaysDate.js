0412fd881483032fdd36ebc2d6c0d25af938aace
Anastasiyaaaa <anastasiya.onishchenk@gmail.com>
2023-01-05 12:27:27 +0200

    test

--- src/TodaysDate.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const TodaysDate = (data) => {
-  console.log(data)
-  return(<div>{`FOO BAR BAZ Today's date is ${data.data}`}</div>)
-}
-
-export default TodaysDate;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-export { default as TodaysDate } from "./TodaysDate";
 export { default as TodaysDate2 } from "./TodaysDate2";
